21ba8f871608f1e0bace48003f5bb81e4ad82608
maksimallist <maksimallist@gmail.com>
2020-03-27 18:46:53 +0300

    style: add comments';

--- embryogenesis/ca_model.py
+++ embryogenesis/ca_model.py
@@ -49,11 +49,11 @@ class CAModel(tf.keras.Model):
     @tf.function
     def perceive(self, x, angle=0.0):
         identify = np.float32([0, 1, 0])
-        identify = np.outer(identify, identify)
-        dx = np.outer([1, 2, 1], [-1, 0, 1]) / 8.0  # Sobel filter
-        dy = dx.T
+        identify = np.outer(identify, identify)  # identify: [[000], [010], [000]];
+        dx = np.outer([1, 2, 1], [-1, 0, 1]) / 8.0  # dx: Sobel filter 'X' [[-1, -2, -1], [000], [1, 2, 1]]/8;
+        dy = dx.T  # dx: Sobel filter 'X' [[1, 2, 1], [000], [-1, -2, -1]]/8;
         c, s = tf.cos(angle), tf.sin(angle)
-        kernel = tf.stack([identify, c * dx - s * dy, s * dx + c * dy], -1)[:, :, None, :]
+        kernel = tf.stack([identify, c * dx - s * dy, s * dx + c * dy], -1)[:, :, None, :]  # todo: wtf ?!
         kernel = tf.repeat(kernel, self.channel_n, 2)
         y = tf.nn.depthwise_conv2d(x, kernel, [1, 1, 1, 1], 'SAME')
 
