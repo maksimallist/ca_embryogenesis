3d411a5423897d4294adfafff9df2d21165217e0
maksimallist <maksimallist@gmail.com>
2020-06-10 16:04:57 +0300

    refactor: __init__ method in PetriDish class (not stable);

--- embryogenesis/model/cellar_automata.py
+++ embryogenesis/model/cellar_automata.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf  # TensorFlow version >= 2.0
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Conv2D, Layer
-
+from typing import Tuple
 
 class StateObservation(Layer):
     def __init__(self,
@@ -114,19 +114,56 @@ class UpdateRule(Model):
 
 
 class PetriDish:  # бывший SamplePool
-    def __init__(self, *, _parent=None, _parent_idx=None, **slots):
+    def __init__(self, channel_n: int,
+                 shape: Tuple[int, int],
+                 live_state_axis: int = 3,
+                 morph_axis: Tuple[int, int] = (0, 2),
+                 *, _parent=None, _parent_idx=None, **slots):
+
         self._parent = _parent
         self._parent_idx = _parent_idx
         self._slot_names = slots.keys()
         self._size = None
+
         for k, v in slots.items():
             if self._size is None:
                 self._size = len(v)
             assert self._size == len(v)
             setattr(self, k, np.asarray(v))
 
-    def sample(self, n):
-        idx = np.random.choice(self._size, n, False)
+
+
+        self.channel_n = channel_n
+        self.height, self.width = shape
+
+        self.live_state_axis = live_state_axis
+        self.morph_axis = morph_axis
+        self.additional_axis = (self.live_state_axis + 1, self.channel_n - 1)
+
+        self.cells_seed = self.make_seed()
+
+    def make_seed(self):
+        cells = np.zeros([self.height, self.width, self.channel_n], np.float32)
+        cells[self.height // 2, self.width // 2, self.morph_axis[1] + 1:] = 1.0
+        return cells
+
+    # todo: убрать отсюда tensorflow
+    @tf.function
+    def make_circle_masks(self, n: int):
+        """ Для повреждений в демо """
+        x = tf.linspace(-1.0, 1.0, self.width)[None, None, :]
+        y = tf.linspace(-1.0, 1.0, self.height)[None, :, None]
+
+        center = tf.random.uniform([2, n, 1, 1], -0.5, 0.5)
+        r = tf.random.uniform([n, 1, 1], 0.1, 0.4)
+
+        x, y = (x - center[0]) / r, (y - center[1]) / r
+        mask = tf.cast(x * x + y * y < 1.0, tf.float32)
+
+        return mask
+
+    def sample(self, batch_size: int):
+        idx = np.random.choice(self._size, batch_size, False)
         batch = {k: getattr(self, k)[idx] for k in self._slot_names}
         batch = PetriDish(**batch, _parent=self, _parent_idx=idx)
         return batch
@@ -134,22 +171,3 @@ class PetriDish:  # бывший SamplePool
     def commit(self):
         for k in self._slot_names:
             getattr(self._parent, k)[self._parent_idx] = getattr(self, k)
-
-    @staticmethod
-    def make_seed(size, channel_n: int, batch_size: int = 1):
-        x = np.zeros([batch_size, size, size, channel_n], np.float32)
-        # todo: сделать выбор осей через аргумент функции или self
-        x[:, size // 2, size // 2, 3:] = 1.0
-        return x
-
-    @staticmethod
-    @tf.function
-    def make_circle_masks(n, h, w):
-        """ Для повреждений в демо """
-        x = tf.linspace(-1.0, 1.0, w)[None, None, :]
-        y = tf.linspace(-1.0, 1.0, h)[None, :, None]
-        center = tf.random.uniform([2, n, 1, 1], -0.5, 0.5)
-        r = tf.random.uniform([n, 1, 1], 0.1, 0.4)
-        x, y = (x - center[0]) / r, (y - center[1]) / r
-        mask = tf.cast(x * x + y * y < 1.0, tf.float32)
-        return mask
