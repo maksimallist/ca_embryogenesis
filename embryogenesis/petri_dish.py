a708028851e83237a439d3de7f9ca8b608cb652f
maksimallist <maksimallist@gmail.com>
2020-06-17 23:16:17 +0300

    refactor: optimize petri_dish.py;

--- embryogenesis/petri_dish.py
+++ embryogenesis/petri_dish.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional, Union
+from typing import Optional, Tuple, Union
 
 import numpy as np
 
@@ -10,13 +10,13 @@ class PetriDish:
                  channel_n: int,
                  pool_size: int = 1024,
                  live_state_axis: int = 3,
-                 morph_axis: Tuple[int, int] = (0, 2)):
+                 image_axis: Tuple[int, int] = (0, 2)):
         # cells shape
         self.height, self.width, self.channel_n = height, width, channel_n
 
         # axis description
         self.live_state_axis = live_state_axis
-        self.morph_axis = morph_axis
+        self.image_axis = image_axis
         self.additional_axis = (self.live_state_axis + 1, self.channel_n - 1)
 
         # main attributes
@@ -26,24 +26,24 @@ class PetriDish:
 
     def make_seed(self, return_seed: bool = False) -> Union[None, np.array]:
         self.cells = np.zeros([self.height, self.width, self.channel_n], np.float32)
-        self.cells[self.height // 2, self.width // 2, self.morph_axis[1] + 1:] = 1.0
-
+        # put one life cell in center of dish
+        self.cells[self.height // 2, self.width // 2, self.image_axis[1] + 1:] = 1.0
         if return_seed:
             return self.cells
 
-    def create_petri_dish(self, return_dish: bool = False, pool_size: Optional[int] = None) -> Union[None, np.array]:
+    def create_petri_dish(self, pool_size: Optional[int] = None, return_dish: bool = False) -> Union[None, np.array]:
         self.make_seed()
-
         if pool_size and return_dish:
             return np.repeat(self.cells[None, ...], repeats=pool_size, axis=0)
         else:
+            # shape of petri_dish [pool_size, height, width, channel_n]
             self.petri_dish = np.repeat(self.cells[None, ...], repeats=self.pool_size, axis=0)
 
     def sample(self, batch_size: int = 32) -> Tuple[np.array, np.array]:
         if self.petri_dish is None:
             self.create_petri_dish()
 
-        batch_idx = np.random.choice(self.pool_size, batch_size, False)
+        batch_idx = np.random.choice(self.pool_size, size=batch_size, replace=False)
         batch = self.petri_dish[batch_idx]
 
         return batch, batch_idx
