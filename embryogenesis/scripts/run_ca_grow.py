af108d4638eb876a838ff2558efc382f45e062ce
maksimallist <maksimallist@gmail.com>
2020-06-18 08:45:19 +0300

    refactor: add anime experiment;

--- embryogenesis/scripts/run_ca_grow.py
+++ embryogenesis/scripts/run_ca_grow.py
@@ -5,12 +5,12 @@ from embryogenesis.cellar_automata import MorphCA
 
 os.environ['FFMPEG_BINARY'] = 'ffmpeg'
 main_root = Path("/home/mks/work/projects/cellar_automata_experiments")
-checkpoints = main_root.joinpath('experiments', 'salamander_2_1000_20200617-234653', 'checkpoints', '1000')
-video_save_path = main_root.joinpath('experiments', 'salamander_2_1000_20200617-234653')
+checkpoints = main_root.joinpath('experiments', 'anime_test_20200618-031208', 'checkpoints', '8000')
+video_save_path = main_root.joinpath('experiments', 'anime_test_20200618-031208')
 
 cellar_automata = MorphCA(rule_model_path=checkpoints,
                           write_video=True,
-                          video_name='salamander_2_mini',
+                          video_name='anime_test_2',
                           save_video_path=video_save_path)
 
-cellar_automata.run_growth(steps=500, return_state=False)
+cellar_automata.run_growth(steps=450, return_state=False)
