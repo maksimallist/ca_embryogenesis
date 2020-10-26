afa7413f221a6de5e25f777c26005d941edd2db8
maksimallist <maksimallist@gmail.com>
2020-10-26 18:22:55 +0300

    refactor: add setup file;

---
+++ _meta.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.0'
+__author__ = 'Petrov Maksim Andreevich'
+__description__ = 'An open source library for building and training cellar automates for embryogenesis simulation.'
+__keywords__ = ['embryogenesis', 'cellar automates', 'biological inspired learning']
+__license__ = 'Apache License, Version 2.0'
+__email__ = 'maksimallist@gmail.com'
---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+import re
+
+from setuptools import setup, find_packages
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+meta_path = os.path.join(__location__, 'ca_embryogenesis', '_meta.py')
+with open(meta_path) as meta:
+    exec(meta.read())
+
+
+def read_requirements():
+    """parses requirements from requirements.txt"""
+    requirements_path = os.path.join(__location__, 'requirements.txt')
+    with open(requirements_path, encoding='utf8') as f:
+        requirements = [line.strip() for line in f if not line.strip().startswith('#')]
+
+    names = []
+    links = []
+    for req in requirements:
+        if '://' in req:
+            links.append(req)
+        else:
+            names.append(req)
+
+    return {'install_requires': names, 'dependency_links': links}
+
+
+def readme():
+    with open(os.path.join(__location__, 'README.md'), encoding='utf8') as f:
+        text = f.read()
+    text = re.sub(r']\((?!https?://)', r'](https://github.com/maksimallist/ca_embryogenesis/master', text)
+    # text = re.sub(r'\ssrc="(?!https?://)', r' src="https://raw.githubusercontent.com/deepmipt/DeepPavlov/master/', text)
+    return text
+
+
+if __name__ == '__main__':
+    setup(
+        name='ca_embryogenesis',
+        packages=find_packages(exclude=('tests', 'docs', 'utils')),
+        version=__version__,
+        description=__description__,
+        long_description=readme(),
+        long_description_content_type='text/markdown',
+        author=__author__,
+        author_email=__email__,
+        license=__license__,
+        url='https://github.com/maksimallist/ca_embryogenesis',
+        # download_url=f'https://github.com/deepmipt/DeepPavlov/archive/{__version__}.tar.gz',
+        keywords=__keywords__,
+        include_package_data=True,
+        **read_requirements()
+    )
