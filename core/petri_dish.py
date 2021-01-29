71921432664bf60a3ba94420e76646f4970c194f
maksimallist <maksimallist@gmail.com>
2021-01-29 13:39:38 +0300

    refactor: start to refactor class of petri dish;

--- core/petri_dish.py
+++ core/petri_dish.py
@@ -50,3 +50,44 @@ class PetriDish:
 
     def commit(self, batch_cells: np.array, cells_idx: np.array) -> None:
         self.set_of_petri_dishes[cells_idx] = batch_cells
+
+
+# class PetriDish:
+#     """
+#     Чашка петри представляет из себя сетку определенного масштаба, состоящую из отдельных "клеток". Масштаб/размер сетки
+#     задаётся аргументами "height" и "height", при объявлении объекта класса. Состояние одной клетки описывается вектором
+#     заданной длины. Три компоненты этого вектора отвечают за значения rgb каналов отдельного пикселя на изображении.
+#     Еще одна компонента отвечает за отслеживание того, какие клетки являются живыми, а какие нет. И соответственно
+#     принимает только значения 0/1. Оставшиеся компоненты не имеют явной интерпретации, их количество может быть
+#     произвольным.
+#     """
+#     def __init__(self,
+#                  height: int,
+#                  width: int,
+#                  cell_states: int,
+#                  rgb_axis: Tuple[int, int, int] = (0, 1, 2),
+#                  live_axis: int = 3):
+#         # Petri dish shape
+#         self.height, self.width = height, width
+#         assert cell_states > 4
+#         self.channels = cell_states
+#
+#         assert live_axis not in rgb_axis
+#         self.live_axis = live_axis
+#         self.rgb_axis = rgb_axis
+#         self.petri_dish = np.zeros([self.height, self.width, self.channels], np.float32)
+#
+#     def cell_state_initialization(self,
+#                                   coordinates: Optional[Tuple[int, int]] = None,
+#                                   return_dish: bool = False) -> Union[None, np.array]:
+#         if coordinates is None:
+#             # put one life cell in center of dish
+#             self.petri_dish[self.height // 2, self.width // 2, self.live_axis:] = 1.0
+#         else:
+#             x, y = coordinates
+#             assert x <= self.width
+#             assert y <= self.height
+#             self.petri_dish[x, y, self.live_axis:] = 1.0
+#
+#         if return_dish:
+#             return self.petri_dish
