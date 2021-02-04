88ca19998f81ebff1ced5d59a790873e19b6a524
maksimallist <maksimallist@gmail.com>
2021-02-04 21:04:13 +0300

    refactor: optimized code of ExperimentWatcher class;

--- core/experiment.py
+++ core/experiment.py
@@ -4,9 +4,10 @@ from typing import Tuple, Optional, Callable
 
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 from tensorflow.keras import Model
 
-from core.image_utils import visualize_batch, generate_pool_figures, to_rgb
+from core.image_utils import to_rgb
 
 
 @tf.function  # TODO: вынести перевод в rgba формат во вне тела функции
@@ -22,14 +23,15 @@ class ExperimentWatcher:
                  target: np.array):
         # experiments infrastructure attributes
         self.root = root
-        self.exp_name = exp_name + '_' + datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.exp_name = exp_name + '_' + datetime.now().strftime("%d.%m.%Y-%H.%M")
         self.target = target
         self.checkpoints_folder = None
         self.pictures_folder = None
         self.last_pictures_folder = None
         self.tensorboard_logs = None
+        self.experiments_preparation()
 
-    def prepare_exp_folder(self):
+    def create_folders(self):
         exp_root = self.root.joinpath(self.exp_name)
         exp_root.mkdir(parents=True, exist_ok=False)
 
@@ -42,44 +44,64 @@ class ExperimentWatcher:
         self.tensorboard_logs = exp_root.joinpath('tensorboard_logs')
         self.tensorboard_logs.mkdir()
 
+    def create_summary_writer(self):
         file_writer = tf.summary.create_file_writer(str(self.tensorboard_logs))
         file_writer.set_as_default()
-        # Using the file writer, log the target image.
-        tf.summary.image("Target image", to_rgb(self.target)[None, ...], step=0)
 
-    def create_rule_imprint(self, trainable_rule: Model, train_step: int, pre_state: np.array, post_state: np.array):
-        model_path = self.checkpoints_folder.joinpath(str(train_step))
+        # Using the file writer, log the target image.
+        # todo: move 'to_rgb' function
+        target_image = to_rgb(self.target)[None, ...]
+        tf.summary.image("Target image", target_image, step=0)
+
+    def experiments_preparation(self):
+        self.create_folders()
+        self.create_summary_writer()
+
+    def save_model(self, trainable_rule: Model, train_step: int):
+        model_path = self.checkpoints_folder.joinpath("train_step_" + str(train_step))
         model_path.mkdir()
         trainable_rule.save(filepath=str(model_path), overwrite=True, save_format="tf")
 
-        self.last_pictures_folder = self.pictures_folder.joinpath(str(train_step))
+    # TODO: upgrade
+    def save_ca_state_as_image(self, train_step: int, post_state: np.array, max_img_count: int = 5):
+        self.last_pictures_folder = self.pictures_folder.joinpath("train_step_" + str(train_step))
         self.last_pictures_folder.mkdir()
-        visualize_batch(pre_state=pre_state,
-                        post_state=post_state,
-                        train_step=train_step,
-                        save_path=str(self.last_pictures_folder),
-                        jupyter=False)
 
-    def log(self, step, loss, petri_dish, trainable_rule, previous_state_batch, next_state_batch):
-        if step % 100 == 0:
-            self.create_rule_imprint(trainable_rule=trainable_rule,
-                                     train_step=step,
-                                     pre_state=previous_state_batch,
-                                     post_state=next_state_batch)
+        path = open(str(self.last_pictures_folder) + f"/batches_{train_step}.jpeg", 'wb')
 
-            tf.summary.scalar('loss_log', data=np.log10(loss), step=step)
-            tf.summary.image("Example of CA figures", to_rgb(previous_state_batch[0])[None, ...], step=step)
+        image = np.hstack(to_rgb(post_state)[:max_img_count])
+        image = np.uint8(np.clip(image, 0, 1) * 255)
+        image = Image.fromarray(image)
+        image.save(path, 'jpeg', quality=95)
 
+        tf.summary.image("Example of CA figures", to_rgb(post_state[0])[None, ...], step=train_step)
+
+    # TODO: fix
+    # def generate_pool_figures(pool_states: np.array, train_step: int, save_path: str) -> None:
+    #     tiled_pool = tile2d(to_rgb(pool_states[:49]))
+    #     fade = np.linspace(1.0, 0.0, 72)
+    #     ones = np.ones(72)
+    #     tiled_pool[:, :72] += (-tiled_pool[:, :72] + ones[None, :, None]) * fade[None, :, None]
+    #     tiled_pool[:, -72:] += (-tiled_pool[:, -72:] + ones[None, :, None]) * fade[None, ::-1, None]
+    #     tiled_pool[:72, :] += (-tiled_pool[:72, :] + ones[:, None, None]) * fade[:, None, None]
+    #     tiled_pool[-72:, :] += (-tiled_pool[-72:, :] + ones[:, None, None]) * fade[::-1, None, None]
+    #
+    #     image_save(save_path + f"/{train_step}_pool.jpg", tiled_pool)
+
+    def log(self, step, loss, trainable_rule, next_state_batch):
         if step % 10 == 0:
-            pool_states = petri_dish.set_of_petri_dishes
-            pool_figures = generate_pool_figures(pool_states=pool_states,
-                                                 train_step=step,
-                                                 save_path=str(self.last_pictures_folder),
-                                                 return_pool=True)
+            tf.summary.scalar('loss_log', data=np.log10(loss), step=step)
 
-            tf.summary.image("Pool CA figures", pool_figures[None, ...], max_outputs=25, step=step)
+        if step % 100 == 0:
+            print(f"\r step: {step}, log10(loss): {np.round(np.log10(loss), decimals=3)}", end='')
 
-        print(f"\r step: {step}, log10(loss): {np.round(np.log10(loss), decimals=3)}", end='')
+        if step % 1000 == 0:
+            self.save_model(trainable_rule, step)
+            self.save_ca_state_as_image(step, next_state_batch, max_img_count=5)
+            # pool_figures = generate_pool_figures(pool_states=pool_states,
+            #                                      train_step=step,
+            #                                      save_path=str(self.last_pictures_folder),
+            #                                      return_pool=True)
 
 
 class TFKerasTrainer:
