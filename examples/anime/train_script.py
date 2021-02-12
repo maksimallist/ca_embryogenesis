be16dff808deb71040c5273ac0a16b2a146981fa
maksimallist <maksimallist@gmail.com>
2021-02-12 13:24:38 +0300

    refactor: add new code in salamander example;

--- examples/anime/train_script.py
+++ examples/anime/train_script.py
@@ -2,11 +2,13 @@ import json
 from pathlib import Path
 
 import numpy as np
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
 
+from core.experiment import l2_loss, ExperimentWatcher, TFKerasTrainer
 from core.image_utils import open_image
-from core.petri_dish import PetriDish
-from core.rule_model import UpdateRule
-from core.rule_trainer import UpdateRuleTrainer
+from core.petri_dish import CADataGenerator, PetriDish
+from core.rule_model import SimpleUpdateModel
 
 if __name__ == '__main__':
     main_root = Path(__file__).parent.absolute()
@@ -16,62 +18,58 @@ if __name__ == '__main__':
     with open(experiment_config, 'r') as conf:
         config = json.load(conf)
 
-    # get target image
-    target_path = str(main_root.joinpath('anime_girl.png'))
-    target_img = open_image(target_path, max_size=40)
+    target_img = open_image(str(main_root.joinpath('anime_girl.png')), max_size=40)
 
     # pad target image to
     target_padding = config['target_padding']
     target_padding_map = ((target_padding, target_padding), (target_padding, target_padding), (0, 0))
     padded_target = np.pad(target_img, target_padding_map, 'constant', constant_values=0.0)
     image_height, image_width = padded_target.shape[:2]
+    print(f"image_height: {image_height}; image_width: {image_width};")
 
-    # create object that will creates CA cells grids
-    sampler = PetriDish(height=image_height,
-                        width=image_width,
-                        channel_n=config['ca_params']['channel_n'],
-                        pool_size=config['ca_params']['pool_size'],
-                        live_state_axis=config['ca_params']['live_state_axis'],
-                        image_axis=tuple(config['ca_params']['image_axis']))
+    ca = PetriDish(height=image_height,  # 56
+                   width=image_width,  # 56
+                   cell_states=config['channel_n'],  # 16
+                   rgb_axis=config['image_axis'],  # (0, 1, 2),
+                   live_axis=config['live_state_axis'])  # 3
+    ca.cell_state_initialization()
 
-    # create network that determine CA update rule
-    model = UpdateRule(name='anime',
-                       channel_n=config['ca_params']['channel_n'],
-                       fire_rate=config['update_rule']['cell_fire_rate'],
-                       life_threshold=config['update_rule']['life_threshold'],
-                       conv_1_filters=config['update_rule']['conv_1_filters'],
-                       conv_kernel_size=config['update_rule']['conv_kernel_size'],
-                       step_size=config['update_rule']['step_size'])
+    data_generator = CADataGenerator(ca_tensor=ca.cells_tensor,
+                                     target=padded_target,
+                                     set_size=config['pool_size'],  # 1024
+                                     damage_n=config['damage_n'],  # 3
+                                     reseed_batch=config['reseed_batch'])  # True
 
-    # determine CA model type and experiments conditions
-    exp_map = config['experiment_map']
-    training_mode = config['experiment_type']
-    use_pattern_pool = exp_map[training_mode]['use_pattern_pool']
-    damage_n = exp_map[training_mode]['damage_n']
+    model = SimpleUpdateModel(name="paper_model",
+                              channels=config['channel_n'],  # 16
+                              life_threshold=config['life_threshold'],  # 0.1
+                              live_axis=config['live_state_axis'],  # 3
+                              perception_kernel_size=config['perception_kernel_size'],  # 3
+                              perception_kernel_type=config['perception_kernel_type'],  # 'sobel'
+                              perception_kernel_norm_value=config['perception_kernel_norm_value'],  # 8
+                              observation_angle=config['observation_angle'],  # 0.0
+                              last_conv_filters=config['last_conv_filters'],  # 128
+                              last_conv_kernel_size=config['last_conv_kernel_size'],  # 1
+                              stochastic_update=config['stochastic_update'],  # True
+                              fire_rate=config['cell_fire_rate'])  # 0.5
 
-    # create trainer for UpdateRule object
-    batch_size = config["train_config"]["batch_size"]
-    train_steps = config["train_config"]["train_steps"]
-    learning_rate = config["train_config"]["learning_rate"]
-    boundaries = config["train_config"]["boundaries"]
-    lr_multiplier = config["train_config"]["lr_multiplier"]
-    grad_norm_value = config["train_config"]["grad_norm_value"]
-    train_ca_step_range = tuple(config["train_config"]["train_ca_step_range"])
+    boundaries_decay_values = [config['learning_rate'], config['learning_rate'] * config['lr_multiplier']]
+    lr_scheduler = PiecewiseConstantDecay(boundaries=[config['boundaries']],
+                                          values=boundaries_decay_values)
+    model_optimizer = Adam(lr_scheduler)
+    model.compile(optimizer=model_optimizer)  # , loss=l2_loss
 
-    trainer = UpdateRuleTrainer(root=main_root,
-                                exp_name='anime_regen',
-                                petri_dish=sampler,
-                                rule_model=model,
-                                target_image=padded_target,
-                                use_pattern_pool=use_pattern_pool,
-                                damage_n=damage_n,
-                                batch_size=batch_size,
-                                train_steps=train_steps,
-                                learning_rate=learning_rate,
-                                boundaries=boundaries,
-                                lr_multiplier=lr_multiplier,
-                                train_ca_step_range=train_ca_step_range,
-                                grad_norm_value=grad_norm_value)
+    watcher = ExperimentWatcher(root=main_root,
+                                exp_name=config['exp_name'],
+                                target=padded_target)
 
-    # run training
-    trainer.train()
+    trainer = TFKerasTrainer(data_generator=data_generator,
+                             model=model,
+                             optimizer=model_optimizer,
+                             watcher=watcher,
+                             loss_function=l2_loss)
+
+    trainer.train(train_steps=config['train_steps'],  # 8000
+                  batch_size=config['batch_size'],  # 8
+                  grad_norm_value=config['grad_norm_value'],
+                  grow_steps=tuple(config['train_ca_step_range']))
